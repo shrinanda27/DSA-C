ac7a76355ac07b2fa98007d5772839572b3649b1
Shrinanda Dinde <shrinanda.cs23@bmsce.ac.in>
2024-10-23 22:48:18 +0530

    updated code for stack operations

--- stackoperations.c
+++ stackoperations.c
@@ -2,120 +2,109 @@
 #include<stdlib.h>
 #include<stdbool.h>
 
-struct stack{
+struct stack {
     int size;
     int top;
     int *arr;
 };
 
-bool isEmpty(struct stack *ptr){
-    if(ptr->top==-1){
-        return true;
-    }
-    else{
-        return false;
-    }
-
+bool isEmpty(struct stack *ptr) {
+    return ptr->top == -1;
 }
 
-bool isFUll(struct stack *ptr){
-    if(ptr->top==ptr->size-1){
-        return true;
-    }
-    else{
-        return false;
-    }
+bool isFull(struct stack *ptr) {
+    return ptr->top == ptr->size - 1;
 }
 
-// push
-void push(int value,struct stack *ptr){
-    if(isFUll(ptr)){
-        printf("Stack OverFlow!! cannot push %d into the stack\n",value);
+void push(int value, struct stack *ptr) {
+    if (isFull(ptr)) {
+        printf("Stack OverFlow!! cannot push %d into the stack\n", value);
         return;
-    }
-    else{
+    } else {
         ptr->top++;
-        ptr->arr[ptr->top]=value;
-        printf("%d is pushed into the stack.\n",value);
+        ptr->arr[ptr->top] = value;
+        printf("%d is pushed into the stack.\n", value);
     }
 }
 
-// pop
-int pop(struct stack *ptr){
-    if(isEmpty(ptr)){
+int pop(struct stack *ptr) {
+    if (isEmpty(ptr)) {
         printf("Stack UnderFlow!! cannot pop an element from the stack\n");
         return -1;
-    }
-    else{
-        int value=ptr->arr[ptr->top];
-        ptr->top=ptr->top-1;
-        printf("%d is popped from the stack.\n",value);
+    } else {
+        int value = ptr->arr[ptr->top];
+        ptr->top--;
+        printf("%d is popped from the stack.\n", value);
         return value;
     }
 }
 
-// peek
-int peek(struct stack *ptr,int i){
-    int arrayIndex=ptr->top-i+1;
-    if(arrayIndex<0){
+int peek(struct stack *ptr, int i) {
+    int arrayIndex = ptr->top - i + 1;
+    if (arrayIndex < 0) {
         printf("Invalid Position\n");
         return -1;
-    }
-    else{
+    } else {
         return ptr->arr[arrayIndex];
     }
-
 }
 
-// stack top
-int stacktop(struct stack *ptr){
+void display(struct stack *ptr) {
+    if (isEmpty(ptr)) {
+        printf("Stack is empty.\n");
+        return;
+    }
+    printf("Stack elements: ");
+    for (int i = 0; i <= ptr->top; i++) {
+        printf("%d ", ptr->arr[i]);
+    }
+    printf("\n");
+}
+
+int stacktop(struct stack *ptr) {
     return ptr->arr[ptr->top];
 }
 
-// stack bottom
-int stackbottom(struct stack *ptr){
+int stackbottom(struct stack *ptr) {
     return ptr->arr[0];
 }
 
-
-void main(){
-    struct stack *s=(struct stack*)malloc(sizeof(struct stack));
-    s->size=80;
-    s->top=-1;
-    s->arr=(int *)malloc(s->size*sizeof(int));
+void main() {
+    struct stack *s = (struct stack *)malloc(sizeof(struct stack));
+    s->size = 80;
+    s->top = -1;
+    s->arr = (int *)malloc(s->size * sizeof(int));
     printf("Stack has been created successfully!\n");
 
-    if(isEmpty(s)){
-        printf("stack is empty\n");
+    int choice, element, i;
+
+    while (1) {
+        printf("\nOptions: 1. Push  2. Pop  3. Peek  4. Display  5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter the element to push: ");
+                scanf("%d", &element);
+                push(element, s);
+                break;
+            case 2:
+                pop(s);
+                break;
+            case 3:
+                printf("Enter the position to peek: ");
+                scanf("%d", &i);
+                printf("Element at position %d is %d\n", i, peek(s, i));
+                break;
+            case 4:
+                display(s);
+                break;
+            case 5:
+                printf("Exiting...\n");
+                exit(0);
+            default:
+                printf("Invalid choice! Please try again.\n");
+        }
     }
-    else{
-        printf("stack is not empty\n");
-    }
-
-    printf("-------------------------------------------------------------------------------------\n");
-
-    push(5,s);
-    int VAL=pop(s);
-    printf("%d\n",VAL);
-
-    push(6,s);
-    push(7,s);
-    push(8,s);
-
-    int value=peek(s,3);
-    printf("%d\n",value);
-
-// to print stack elements
-    for(int j=0;j<=s->top;j++){
-        printf("The value at index %d is %d",j,peek(s,j+1));
-        printf("\n");
-    }
-
-    // stack top and bottom
-    printf("stack top element: %d\n",stacktop(s));
-    printf("stack bottom element: %d\n",stackbottom(s));
-
-
-
-    
 }
